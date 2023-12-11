505cc7efed01f208d02b93e9bc24d75007a09174
Takeru Miyake <m18kecam777@gmail.com>
2023-12-11 20:27:21 +0900

    init

---
+++ study-visionpro/study-visionpro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  study-visionpro
+//
+//  Created by 三宅武将 on 2023/12/11.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ study-visionpro/study-visionpro/study_visionproApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_visionproApp.swift
+//  study-visionpro
+//
+//  Created by 三宅武将 on 2023/12/11.
+//
+
+import SwiftUI
+
+@main
+struct study_visionproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
